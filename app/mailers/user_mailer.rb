9cb72ea5b7dbb166c477b2dd1046612d10bf3593
濱野将士 <>
2018-04-18 18:01:29 +0900

    second commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: 'noreply@example.com'
+
+  def welcome_mail(user)
+    @user = user
+    mail(to: @user.email, subject: 'ご登録ありがとうございます')
+  end
+
+end
